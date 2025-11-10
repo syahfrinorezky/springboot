6a85936a8df8d653c1b50bfb7b596623578f8a15
rinorezky <syahfrinorezky@gmail.com>
2025-11-11 01:39:58 +0800

    feat: add new controller for test

---
+++ src/main/java/com/example/springboot/Controller/HelloController.java
@@ -0,0 +1,12 @@
+package com.example.springboot.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/")
+    public String index() {
+        return "Hello, World!";
+    }
+}
